a93c07c5f3cc8ce5c44903e967cb73e9ad3cee60
sasamile <nspes2020@gmail.com>
2025-02-17 21:14:23 -0500

    update button menu

--- src/components/common/button-menu.tsx
+++ src/components/common/button-menu.tsx
@@ -4,8 +4,13 @@ import { useState } from "react"
 export function ButtonMenu() {
   const [isActive, setIsActive] = useState(false)
 
+  const handleClick = () => {
+    setIsActive(!isActive)
+  }
+
   return (
     <div>
+      <button onClick={handleClick}>Toggle</button>
       <div className={cn(isActive && "")}></div>
     </div>
   )
